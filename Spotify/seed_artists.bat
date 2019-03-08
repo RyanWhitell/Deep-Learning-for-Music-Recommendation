0742e7b01fa4588c5df07042761b7c3be0270bcf
Ryan Whitell <ryanwhitell@hotmail.com>
2019-03-08 14:22:23 -0800

    Remove most initial seeds. Leave only a few popular genres.

--- Spotify/seed_artists.bat
+++ Spotify/seed_artists.bat
@@ -1,245 +1,87 @@
-:: Basshunter 'dance pop', 'eurodance', 'europop'
-python scrape.py -s=37DdwREpvvQHmGLFEZ4h0Q
-
-:: The Chainsmokers 'dance pop', 'edm', 'electropop', 'pop', 'tropical house'
-python scrape.py -s=69GGBxA162lTqCwzJG5jLp
-
-:: Carly Rae Jepsen 'canadian pop', 'dance pop', 'electropop', 'indie poptimism', 'pop', 'post-teen pop'
-python scrape.py -s=6sFIWsNpZYqfjUpaCgueju
-
-:: Macklemore & Ryan Lewis 'dance pop', 'pop', 'pop rap'
-python scrape.py -s=5BcAKTbp20cv7tC5VqPFoC
-
-:: PSY 'dance pop', 'k-hop', 'k-pop'
-python scrape.py -s=2dd5mrQZvg6SmahdgVKDzh
-
-:: Céline Dion 'canadian pop', 'dance pop'
-python scrape.py -s=4S9EykWXhStSc15wEx8QFK
-
-:: Grizzly Bear 'alternative dance', 'art pop', 'brooklyn indie', 'chamber pop', 'chillwave', 'dream pop', 'folk-pop', 'freak folk', 'indie folk', 'indie pop', 'indie rock', 'indietronica', 'lo-fi', 'modern rock', 'new rave', 'new weird america', 'noise pop', 'nu gaze', 'shimmer pop', 'stomp and holler'
-python scrape.py -s=2Jv5eshHtLycR6R8KQCdc4
-
-:: Ariel Pink 'alternative dance', 'alternative rock', 'art pop', 'art rock', 'chillwave', 'dance-punk', 'dream pop', 'experimental pop', 'folk-pop', 'freak folk', 'garage psych', 'indie folk', 'indie garage rock', 'indie pop', 'indie rock', 'indietronica', 'lo-fi', 'modern rock', 'neo-psychedelic', 'new rave', 'new wave', 'new weird america', 'noise pop', 'noise rock', 'nu gaze', 'preverb'
-python scrape.py -s=5H0YoDsPDi9fObFmJtTjfN
-
-:: Backstreet Boys 'boy band', 'dance pop', 'pop'
-python scrape.py -s=5rSXSAkZ67PYJSvpUpkOr7
-
-:: One Direction 'boy band', 'dance pop', 'pop', 'post-teen pop', 'talent show'
-python scrape.py -s=4AK6F7OLvEQ5QYCBNiQWHq
-
-:: N.W.A. 'conscious hip hop', 'gangster rap', 'hardcore hip hop', 'hip hop', 'old school hip hop', 'pop rap', 'rap', 'west coast rap'
-python scrape.py -s=4EnEZVjo3w1cwcQYePccay
-
-:: Run–D.M.C. 'east coast hip hop', 'hardcore hip hop', 'hip hop', 'old school hip hop', 'pop rap', 'rap'
-python scrape.py -s=3CQIn7N5CuRDP8wEI7FiDA
-
-:: Beastie Boys 'alternative rock', 'east coast hip hop', 'hip hop', 'old school hip hop', 'rap'
-python scrape.py -s=03r4iKL2g2442PT9n2UKsx
-
-:: Rammstein 'alternative metal', 'german metal', 'industrial', 'industrial metal', 'industrial rock', 'neue deutsche harte', 'nu metal'
-python scrape.py -s=6wWVKhxIU2cEi0K81v7HvP
-
-:: Nine Inch Nails 'alternative metal', 'alternative rock', 'cyberpunk', 'industrial', 'industrial metal', 'industrial rock', 'modern rock', 'nu metal', 'post-grunge', 'rock'
-python scrape.py -s=0X380XXQSNBYuleKzav5UO
-
-:: Grimes 'alternative dance', 'art pop', 'canadian electropop', 'chamber pop', 'chillwave', 'dance pop', 'dream pop', 'electropop', 'escape room', 'grave wave', 'indietronica', 'metropopolis', 'new rave', 'pop'
-python scrape.py -s=053q0ukIDRgzwTr4vNSwab
-
-:: Ariana Grande 'dance pop', 'pop', 'post-teen pop'
-python scrape.py -s=66CXWjxzNUsdJxJ2JdwvnR
-
-:: Iggy Azalea 'australian hip hop', 'dance pop', 'pop', 'pop rap', 'post-teen pop', 'r&b'
-python scrape.py -s=5yG7ZAZafVaAlMTeBybKAL
-
-:: Clean Bandit 'dance pop', 'edm', 'pop', 'post-teen pop', 'tropical house', 'uk funky'
-python scrape.py -s=6MDME20pz9RveH9rEXvrOM
-
-:: Childish Gambino 'atl hip hop', 'hip hop', 'pop rap', 'rap'
-python scrape.py -s=73sIBHcqh3Z3NyqHKZ7FOL
-
-:: The Weeknd 'canadian contemporary r&b', 'canadian pop', 'pop', 'rap'
-python scrape.py -s=1Xyo4u8uXC1ZmMpatF05PJ
-
-:: Neon Trees 'indie pop', 'indie poptimism', 'modern alternative rock', 'modern rock', 'neo mellow', 'pop', 'pop punk', 'pop rock', 'post-teen pop'
-python scrape.py -s=0RpddSzUHfncUWNJXKOsjy
-
-:: Drake 'canadian hip hop', 'canadian pop', 'hip hop', 'pop rap', 'rap'
-python scrape.py -s=3TVXtAsR1Inumwj472S9r4
-
-:: Nelly Furtado 'canadian pop', 'dance pop', 'pop', 'pop rap', 'pop rock', 'post-teen pop'
-python scrape.py -s=2jw70GZXlAI8QzWeY2bgRc
-
-:: Panic! At The Disco 'emo', 'modern rock', 'pop punk'
-python scrape.py -s=20JZFwl6HVl6yg8a4H3ZqK
-
-:: Bloodhound Gang 'alternative metal', 'comic', 'funk metal', 'nu metal', 'pop punk', 'punk', 'rap rock'
-python scrape.py -s=6nDLku5uL3ou60kvCGZorh
-
-:: Dean Brody 'canadian contemporary country', 'canadian country', 'contemporary country', 'country', 'country road', 'modern country rock'
-python scrape.py -s=0ryF1Gy87xVCOF0Jb666ak
-
-:: Tenille Arts 'canadian contemporary country', 'country pop'
-python scrape.py -s=0gSAxSvAApQc71cTpoXDFu
-
-:: JJ Shiplett 'alberta country', 'canadian contemporary country', 'canadian folk'
-python scrape.py -s=0XgzaFjKPFJT7VziwGD6o5
-
-:: Colter Wall 'canadian contemporary country', 'new americana', 'saskatchewan indie'
-python scrape.py -s=3xYXYzm9H3RzyQgBrYwIcx
-
-:: Jade Eagleson 'canadian contemporary country'
-python scrape.py -s=2nTzAHwCk0swkDdIPj2FIP
-
-:: Three Days Grace 'alternative metal', 'canadian metal', 'canadian rock', 'nu metal', 'pop punk', 'post-grunge'
-python scrape.py -s=2xiIXseIJcq3nG7C8fHeBj
-
-:: Alexisonfire 'canadian rock', 'melodic metalcore', 'metalcore', 'post-hardcore', 'screamo'
-python scrape.py -s=53RsXctnNmj9oKXvcbvzI2
-
-:: Bedouin Soundclash 'canadian rock'
-python scrape.py -s=2Au2mS47rMRkqbU4XJqKMh
-
-:: Broken Social Scene 'alternative dance', 'alternative rock', 'canadian indie', 'canadian rock', 'chamber pop', 'chillwave', 'dance-punk', 'dream pop', 'folk-pop', 'freak folk', 'garage psych', 'indie folk', 'indie pop', 'indie rock', 'indietronica', 'lo-fi', 'modern rock', 'noise pop', 'nu gaze', 'post-rock', 'slow core', 'stomp and holler'
-python scrape.py -s=7lOJ7WXyopaxri0dbOiZkd
-
-:: The Tragically Hip 'canadian indie', 'canadian rock', 'deep new americana', 'ontario indie'
-python scrape.py -s=0YMeriqrS3zgsX24nfY0F0
-
-:: Japandroids 'alternative rock', 'dance-punk', 'indie rock', 'lo-fi', 'modern alternative rock', 'modern rock', 'noise pop', 'noise rock', 'vancouver indie', 'vancouver punk'
-python scrape.py -s=2WcCoPxAWFNSCvsFPrpf30
-
-:: Fitz and The Tantrums 'indie pop', 'indie poptimism', 'indietronica', 'la indie', 'modern alternative rock', 'modern rock'
-python scrape.py -s=4AcHt3JxKy59IX7JNNlZn4
-
-:: Dead Posey 'modern alternative rock'
-python scrape.py -s=7qTUKYzO77RvL1Kxie0xUO
-
-:: KALEO 'icelandic indie', 'modern alternative rock', 'modern rock'
-python scrape.py -s=7jdFEYD2LTYjfwxOdlVjmc
-
-:: Sum 41 'alternative metal', 'canadian punk', 'nu metal', 'pop punk', 'post-grunge', 'punk'
-python scrape.py -s=0qT79UgT5tY4yudH9VfsdT
-
-:: illScarlett 'canadian punk'
-python scrape.py -s=7wifMhiQRkNVGDL3habHha
-
-:: The Dirty Nil 'canadian punk', 'indie punk'
-python scrape.py -s=2mGENPk4M4jtaf5D7fDi98
-
-:: The Cranberries 'irish rock', 'pop rock'
-python scrape.py -s=7t0rwkOPGlDPEhaOcVtOt9
-
-:: Garbage 'alternative dance', 'alternative rock', 'dance pop', 'electronic', 'lilith', 'new wave pop', 'permanent wave', 'pop rock', 'post-grunge', 'rock', 'trip hop'
-python scrape.py -s=6S0GHTqz5sxK5f9HtLXn9q
-
-:: Neutral Milk Hotel 'alternative rock', 'anti-folk', 'athens indie', 'chamber pop', 'e6fi', 'folk-pop', 'freak folk', 'indie folk', 'indie pop', 'indie rock', 'lo-fi', 'modern rock', 'noise pop', 'stomp and holler'
-python scrape.py -s=2ooIqOf4X2uz4mMptXCtie
-
-:: Hot Chip 'alternative dance', 'art pop', 'chillwave', 'dance-punk', 'electronic', 'electropop', 'indie pop', 'indie rock', 'indietronica', 'modern rock', 'neo-synthpop', 'new rave', 'trip hop'
-python scrape.py -s=37uLId6Z5ZXCx19vuruvv5
-
-:: Mars Volta 'alternative metal', 'alternative rock', 'el paso indie', 'funk rock', 'garage rock', 'modern rock'
-python scrape.py -s=75U40yZLLPglFgXbDVnmVs
-
-:: DILLY DALLY 'canadian shoegaze', 'etherpop', 'indie punk', 'preverb'
-python scrape.py -s=6HcTkJKjlZEJgv3fR4CJCS
-
-:: Cancer Bats 'canadian hardcore', 'canadian metal', 'canadian punk'
-python scrape.py -s=10YNQq86z4shHwDSymTyWc
-
-:: Woods Of Ypres 'atmospheric black metal', 'avantgarde metal', 'canadian black metal', 'canadian metal', 'doom metal', 'gothic metal', 'pagan black metal'
-python scrape.py -s=24dCDrr94SNSAREMJhBHCs
-
-:: Mac DeMarco 'edmonton indie'
-python scrape.py -s=3Sz7ZnJQBIHsXLUSo0OQtM
-
-:: Christian Hansen & the Autistics 'edmonton indie'
-python scrape.py -s=1unc1cvxzcISorzSfsnIMb
-
-:: Foals 'alternative dance', 'dance-punk', 'garage rock', 'indie pop', 'indie rock', 'indietronica', 'math pop', 'modern alternative rock', 'modern rock', 'new rave', 'rock'
-python scrape.py -s=6FQqZYVfTNQ1pCqfkwVFEa
-
-:: Modest Mouse 'alternative rock', 'garage rock', 'indie folk', 'indie pop', 'indie rock', 'lo-fi', 'modern rock', 'pop rock', 'stomp and holler', 'washington indie'
-python scrape.py -s=1yAwtBaoHLEDWAnWR87hBT
-
-:: Red Hot Chilli Peppers 'alternative rock', 'funk metal', 'permanent wave', 'post-grunge', 'rock'
-python scrape.py -s=0L8ExT028jH3ddEcZwqJJ5
-
-:: John Frusciante 'alternative rock', 'blues-rock', 'rock'
-python scrape.py -s=7rN3Agir6FaZNfhf5V7mfN
-
-:: Neil Young 'album rock', 'classic canadian rock', 'classic rock', 'mellow gold', 'permanent wave', 'psychedelic rock', 'rock', 'roots rock', 'singer-songwriter'
-python scrape.py -s=6v8FB84lnmJs434UJf2Mrm
-
-:: The Beatles 'british invasion', 'merseybeat', 'psychedelic rock', 'rock'
-python scrape.py -s=3WrFJ7ztbogyGnTHbHJFl2
-
-:: The Rolling Stones 'album rock', 'british invasion', 'classic rock', 'protopunk', 'rock'
-python scrape.py -s=22bE4uQ6baNwSHPVcDxLCe
-
-:: The Clash 'classic rock', 'dance rock', 'new wave', 'permanent wave', 'punk', 'rock'
-python scrape.py -s=3RGLhK1IP9jnYFH4BRFJBS
-
-:: Mother Mother 'canadian indie', 'indie pop', 'indie rock', 'modern alternative rock', 'modern rock', 'vancouver indie'
-python scrape.py -s=0e86yPdC41PGRkLp2Q1Bph
-
+:: Rock
 :: Queen 'glam rock', 'rock'
 python scrape.py -s=1dfeR4HaWDbWqFHLkxsg1d
 
-:: David Bowie 'album rock', 'art rock', 'dance rock', 'glam rock', 'new wave', 'permanent wave', 'protopunk', 'rock'
-python scrape.py -s=0oSGxfWSnnOXhD2fKuz2Gy
+:: Alternative Rock
+:: Modest Mouse 'alternative rock', 'garage rock', 'indie folk', 'indie pop', 'indie rock', 'lo-fi', 'modern rock', 'pop rock', 'stomp and holler', 'washington indie'
+python scrape.py -s=1yAwtBaoHLEDWAnWR87hBT
 
-:: Pink Floyd 'album rock', 'art rock', 'classic rock', 'progressive rock', 'psychedelic rock', 'rock', 'symphonic rock'
-python scrape.py -s=0k17h0D3J5VfsdmQ1iZtE9
+:: Jazz
+:: Kenny G 'smooth jazz'
+python scrape.py -s=6I3M904Y9IwgDjrQ9pANiB
 
-:: Tenacious D 'alternative metal', 'comedy rock', 'comic', 'post-grunge', 'rock'
-python scrape.py -s=1XpDYCrUJnvCo9Ez6yeMWh
+:: Metal
+:: In Flames 'alternative metal', 'gothenburg metal', 'groove metal', 'melodic death metal', 'melodic metalcore', 'metal', 'nu metal', 'swedish death metal', 'swedish metal'
+python scrape.py -s=57ylwQTnFnIhJh4nu4rxCs
 
-:: Gordon Lightfoot 'album rock', 'canadian country', 'canadian folk', 'classic rock', 'country rock', 'folk', 'folk rock', 'mellow gold', 'rock', 'roots rock', 'singer-songwriter', 'soft rock', 'traditional folk'
-python scrape.py -s=23rleGXVOVVgTk3xgtmfE4
+:: Blues
+:: Stevie Ray Vaughan 'album rock', 'blues', 'blues-rock', 'classic rock', 'electric blues', 'hard rock', 'instrumental rock', 'modern blues', 'rock', 'southern rock', 'texas blues'
+python scrape.py -s=5fsDcuclIe8ZiBD5P787K1
 
-:: The Jeff Healey Band 'blues-rock', 'canadian blues', 'classic rock', 'electric blues', 'modern blues', 'southern rock'
-python scrape.py -s=3d2hJTVTwo08F9b0ZFQukJ
+:: Hip-Hop
+:: Childish Gambino 'atl hip hop', 'hip hop', 'pop rap', 'rap'
+python scrape.py -s=73sIBHcqh3Z3NyqHKZ7FOL
 
-:: Otis Redding 'christmas', 'classic soul', 'funk', 'memphis soul', 'soul', 'soul blues', 'southern soul'
-python scrape.py -s=60df5JBRRPcnSpsIMxxwQm
+:: Pop
+:: Drake 'canadian hip hop', 'canadian pop', 'hip hop', 'pop rap', 'rap'
+python scrape.py -s=3TVXtAsR1Inumwj472S9r4
 
-:: The Veltones 'memphis soul'
-python scrape.py -s=3XE4BdxHKJ125symgiKX9O
+:: Punk
+:: The Clash 'classic rock', 'dance rock', 'new wave', 'permanent wave', 'punk', 'rock'
+python scrape.py -s=3RGLhK1IP9jnYFH4BRFJBS
 
-:: Billy & The King Bees 'memphis soul'
-python scrape.py -s=2ryTRED9xqbnqK3W5bAQah
-
-:: Aretha Franklin 'adult standards', 'christmas', 'classic soul', 'jazz blues', 'memphis soul', 'soul', 'southern soul'
-python scrape.py -s=7nwUJBm0HE4ZxD3f5cy5ok
-
-:: Isaac Hayes 'classic soul', 'disco', 'funk', 'jazz funk', 'memphis soul', 'motown', 'quiet storm', 'soul', 'southern soul'
-python scrape.py -s=3IKV7o6WPphDB7cCWXaG3E
-
-:: Bob Dylan 'album rock', 'classic rock', 'folk', 'folk rock', 'folk-pop', 'mellow gold', 'rock', 'roots rock', 'singer-songwriter'
-python scrape.py -s=74ASZWbe4lXaubB36ztrGX
-
-:: The Avett Brothers 'alternative country', 'folk-pop', 'indie folk', 'indie pop', 'new americana', 'north carolina indie', 'pop rock', 'progressive bluegrass', 'stomp and holler'
-python scrape.py -s=196lKsA13K3keVXMDFK66q
-
-:: The Devil Makes Three 'new americana', 'progressive bluegrass', 'stomp and holler'
-python scrape.py -s=63knPlGzLHTNDf1J78Fvte
-
-:: Bill Haley & His Comets 'adult standards', 'brill building pop', 'christmas', 'doo-wop', 'rock-and-roll', 'rockabilly'
-python scrape.py -s=3MFp4cYuYtTZe3d3xkLLbr
-
-:: Ben E. King 'adult standards', 'brill building pop', 'christmas', 'motown', 'rock-and-roll', 'rockabilly', 'soul', 'southern soul'
-python scrape.py -s=3plJVWt88EqjvtuB4ZDRV3
-
+:: Rockabilly
 :: Elvis Presley 'adult standards', 'christmas', 'rock-and-roll', 'rockabilly'
 python scrape.py -s=43ZHCT0cAZBISjO8DG9PnE
 
+:: Folk
+:: Bob Dylan 'album rock', 'classic rock', 'folk', 'folk rock', 'folk-pop', 'mellow gold', 'rock', 'roots rock', 'singer-songwriter'
+python scrape.py -s=74ASZWbe4lXaubB36ztrGX
+
+:: Experimental
 :: Frank Zappa ['album rock', 'art rock', 'classic rock', 'experimental', 'experimental rock', 'funk rock', 'hard rock', 'instrumental rock', 'jazz fusion', 'progressive rock', 'protopunk', 'psychedelic rock', 'rock', 'symphonic rock', 'zolo'
 python scrape.py -s=6ra4GIOgCZQZMOaUECftGN
 
-:: Sonic Youth 'alternative rock', 'art rock', 'blues-rock', 'dance rock', 'dance-punk', 'dream pop', 'experimental', 'experimental rock', 'garage rock', 'grunge', 'indie rock', 'lo-fi', 'modern rock', 'new wave', 'noise pop', 'noise rock', 'post-hardcore', 'post-punk', 'psychedelic rock', 'rock'
-python scrape.py -s=5UqTO8smerMvxHYA5xsXb6
+:: Reggae
+:: Bob Marley 'reggae', 'roots reggae'
+python scrape.py -s=2QsynagSdAqZj3U9HgDzjD
 
-:: BADBADNOTGOOD'alternative hip hop', 'canadian modern jazz', 'dark jazz', 'escape room', 'funk', 'hip hop'
-python scrape.py -s=65dGLGjkw3UbddUg2GKQoZ
+:: Electronic
+:: Daft Punk 'electro', 'electronic', 'filter house'
+python scrape.py -s=4tZwfgrHOc3mvqYlEYSvVi
+
+:: Country
+:: Garth Brooks 'contemporary country', 'country'
+python scrape.py -s=4tZwfgrHOc3mvqYlEYSvVi
+
+:: Dance Pop
+:: Macklemore & Ryan Lewis 'dance pop', 'pop', 'pop rap'
+python scrape.py -s=5BcAKTbp20cv7tC5VqPFoC
+
+:: Boy Band
+:: Backstreet Boys 'boy band', 'dance pop', 'pop'
+python scrape.py -s=5rSXSAkZ67PYJSvpUpkOr7
+
+:: Rap
+:: N.W.A. 'conscious hip hop', 'gangster rap', 'hardcore hip hop', 'hip hop', 'old school hip hop', 'pop rap', 'rap', 'west coast rap'
+python scrape.py -s=4EnEZVjo3w1cwcQYePccay
+
+:: Industrial
+:: Nine Inch Nails 'alternative metal', 'alternative rock', 'cyberpunk', 'industrial', 'industrial metal', 'industrial rock', 'modern rock', 'nu metal', 'post-grunge', 'rock'
+python scrape.py -s=0X380XXQSNBYuleKzav5UO
+
+:: Classic Rock
+:: The Rolling Stones 'album rock', 'british invasion', 'classic rock', 'protopunk', 'rock'
+python scrape.py -s=22bE4uQ6baNwSHPVcDxLCe
+
+:: Psychedelic Rock
+:: Pink Floyd 'album rock', 'art rock', 'classic rock', 'progressive rock', 'psychedelic rock', 'rock', 'symphonic rock'
+python scrape.py -s=0k17h0D3J5VfsdmQ1iZtE9
+
+:: Soul
+:: Aretha Franklin 'adult standards', 'christmas', 'classic soul', 'jazz blues', 'memphis soul', 'soul', 'southern soul'
+python scrape.py -s=7nwUJBm0HE4ZxD3f5cy5ok
+
+:: Grunge
+:: Aretha Franklin 'alternative rock', 'grunge', 'permanent wave', 'post-grunge', 'rock'
+python scrape.py -s=6olE6TJLqED3rqDCT0FyPh
