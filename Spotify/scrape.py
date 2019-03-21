133258f05b8a42b8c34ea935ae96dcf5597e5b7f
Ryan Whitell <ryanwhitell@hotmail.com>
2019-03-21 11:24:24 -0700

    Genius lyrics can return as 'yet to be released'

--- Spotify/scrape.py
+++ Spotify/scrape.py
@@ -368,6 +368,8 @@ def get_track_lyrics(song_title, artist_name):
         
     song_title = clean_song_title(song_title)
     
+    ytbr = 'Lyrics for this song have yet to be released. Please check back once the song has been released.'
+    
     printlog('Try Genius...')
     try:
         lyrics = get_lyrics_genius(song_title, artist_name)
@@ -375,6 +377,8 @@ def get_track_lyrics(song_title, artist_name):
             raise Exception('Lyrics empty')
         if len(lyrics) >= 15000:
             raise Exception('Lyrics too big')
+        if ytbr in lyrics:
+            raise Exception('Lyrics yet to be released')
     except Exception:
         printlog('Genius lyrics not found, try LYRICS data...', e=True)
         try:
